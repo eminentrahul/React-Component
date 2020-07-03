9bf7655063803a8d9302e4fcf16cd10738cff270
Rahul Ravi Prakash <eminentrahul@rocketmail.com>
2020-07-04 01:13:06 +0530

    Initial commit

---
+++ src/Components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Cricket</li>
+      <li>Football</li>
+      <li>Badminton</li>
+    </ul>
+  );
+}
+
+export default List;
